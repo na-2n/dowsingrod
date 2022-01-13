fa769ae31507961ea02cc891663f99dc666ab714
yuwui <me@yui.pet>
2022-01-13 01:42:59 +0100

    serialize/deserialize in rediscache

--- dowsingrod/cache/redis.py
+++ dowsingrod/cache/redis.py
@@ -1,3 +1,11 @@
+import sys
+
+try:
+    import ujson as json
+except:
+    import json
+
+from dataclasses import asdict, is_dataclass
 from typing import Any, Optional
 from redis import Redis
 
@@ -11,11 +19,38 @@ class RedisCache(Cache):
         else:
             self._redis = Redis(*args, **kwargs)
 
+    # holy fuck this is cancerous
     def get(self, key: str, default: Optional[Any] = None) -> Any:
-        return self._redis.get(key) or default
+        val = self._redis.get(key)
+        if not val:
+            return default
+
+        obj = json.loads(val)
+
+        modname, tname = obj['_python_type'].rsplit('.', 1)
+        data = obj['_data']
+
+        if modname not in sys.modules:
+            __import__(modname)
+
+        mod = sys.modules[modname]
+        t = getattr(mod, tname)
+
+        if isinstance(data, dict):
+            return t(**data)
+        elif t is not list and isinstance(data, list) :
+            return t(*data)
+        else:
+            return t(data)
 
     def set(self, key: str, value: Any, expire: Optional[int] = None):
-        self._redis.set(key, value, ex=expire)
+        t = type(value)
+        obj = {
+            '_python_type': t.__module__ + '.' + t.__name__,
+            '_data': asdict(value) if is_dataclass(value) else value
+        }
+
+        self._redis.set(key, json.dumps(obj), ex=expire)
 
     def delete(self, key: str):
         self._redis.delete(key)
